2f88fb0be9fd9235fb527b135db4192e7c0efb41
Jiaqi Zou <jzou042@gmail.com>
2025-06-25 18:57:45 -0700

    decouple ingest component from airflow

---
+++ ingest/flask/cygnss_ingest.py
@@ -0,0 +1,41 @@
+import logging
+import os
+import subprocess
+
+logger = logging.getLogger(__name__)
+
+shortname = os.getenv("CYGNSS_SHORTNAME_VERSION")
+
+bucket_path = os.getenv("GCS_BUCKET_PATH")
+raw_cygnss_path = os.getenv('CYGNSS_RAW_PATH')
+download_path = f'{bucket_path}{raw_cygnss_path}'
+
+
+def ingest(datestr, bounding_box='"-180,-90,180,90"', force=False, quiet=False):
+    start = datestr
+    end = datestr
+    if not start.endswith("Z"):
+        start += "T00:00:00Z"
+    if not end.endswith("Z"):
+        end += "T23:59:59Z"
+
+    cmd = (f"podaac-data-downloader -c {shortname} -d {download_path} "
+           f"-b={bounding_box} "
+           f"--start-date {start} --end-date {end}")
+
+    if force:
+        cmd += " --force"
+    if quiet:
+        cmd += " --quiet"
+
+    logger.info("Command: " + cmd)
+    result = subprocess.run(cmd, shell=True)
+
+    if result.returncode == 0:
+        with open(f"{download_path}/success.txt", "w") as f:
+            pass
+        logger.info(f"CYGNSS data download succeeded.")
+    else:
+        logger.error(f"CYGNSS data download failed.")
+        raise Exception("CYGNSS data download failed.")
+
---
+++ ingest/flask/smap_ingest.py
@@ -0,0 +1,44 @@
+import os
+import earthaccess
+import logging
+
+auth = earthaccess.login()
+logger = logging.getLogger(__name__)
+
+short_name = os.getenv("SMAP_SHORTNAME")
+version = os.getenv("SMAP_VERSION")
+
+bucket_path = os.getenv("GCS_BUCKET_PATH")
+raw_smap_path = os.getenv("SMAP_RAW_PATH")
+download_path = f'{bucket_path}{raw_smap_path}'
+
+
+
+def ingest(datestr, bounding_box=(-180, -90, 180, 90)):
+    start = datestr
+    end = datestr
+    if not start.endswith("Z"):
+        start += "T00:00:00Z"
+    if not end.endswith("Z"):
+        end += "T23:59:59Z"
+
+
+    granules = earthaccess.search_data(
+        short_name=short_name,
+        version=version,
+        temporal=(start, end),
+        bounding_box=bounding_box,
+        cloud_hosted=True
+    )
+    files = earthaccess.download(granules, local_path=download_path)
+
+    if len(granules) == len(files) and len(files) != 0:
+        with open(f"{download_path}/success.txt", "w") as f:
+            pass
+        logger.info(f"SMAP data download succeeded.")
+    elif len(granules) == 0:
+        logger.error(f"No smap data found.")
+        raise Exception("No smap data found.")
+    else:
+        logger.error(f"SMAP data download failed.")
+        raise Exception("SMAP data download failed.")
