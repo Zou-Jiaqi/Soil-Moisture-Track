067d9dc9a518953c4015c2d5af06220563695a66
Jiaqi Zou <jzou042@gmail.com>
2025-06-29 05:53:50 -0700

    test cicd pipeline

--- airflow/dags/daily_ingest.py
+++ airflow/dags/daily_ingest.py
@@ -1,97 +1,58 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from airflow.providers.google.cloud.operators.cloud_run import CloudRunExecuteJobOperator
+from airflow.providers.google.cloud.operators.dataproc import DataprocSubmitJobOperator
 from datetime import datetime, timedelta
-from scripts import smap_downloader, cygnss_downloader, smap_ingester, cygnss_ingester, file_uploader, file_cleaner
+from scripts import smap_ingester, cygnss_ingester
+import os
+
+JobName = os.getenv("DOWNLOAD_JOB_NAME")
+ProjectId = os.getenv("PROJECT_ID")
+Region = os.getenv("REGION")
 
 default_args = {
     "owner": "airflow",
+    "dag_id": "soil_moisture_track",
     "retries": 3,
     "retry_delay": timedelta(minutes=60),
-    "start_date": datetime(2025, 6, 3),
+    "start_date": datetime(2025, 1, 1),
 }
 
-# download and ingest the data of 3 days ago in case data source is not ready
-def get_target_date(execution_date):
-    return execution_date - timedelta(days=3)
 
 # schedule interval: executed at 00:00 every day
-with DAG("daily_ingest", default_args=default_args, schedule_interval="0 0 * * *", catchup=True) as dag:
+with DAG("data_pipeline", default_args=default_args, schedule_interval="0 0 * * *", catchup=True) as dag:
 
-    def download_smap(**context):
-        date = get_target_date(context["execution_date"]).strftime("%Y-%m-%d")
-        smap_downloader.download(date, date)
-
-
-    def ingest_smap(**context):
-        filedate = get_target_date(context["execution_date"]).date()
-        smap_ingester.ingest(filedate)
+    # target date = current date - 3
+    def get_target_date(**context):
+        return context["execution_date"] - timedelta(days=3)
 
 
-    def archive_smap(**context):
-        filedate = get_target_date(context["execution_date"]).date()
-        file_uploader.upload_smap_raw(filedate)
+    satellite_data_download_task = CloudRunExecuteJobOperator(
+        task_id="download_satellite_data",
+        project_id=ProjectId,
+        region=Region,
+        job_name=JobName,
+        overrides={
+            "environment": {
+                "variables": {
+                    "DOWNLOAD_DATE": get_target_date().strftime("%Y-%m-%d"),
+                }
+            }
+        }
+    )
 
+    PYSPARK_JOB = {
+        "reference": {"project_id": "your-project-id"},
+        "placement": {"cluster_name": "your-cluster-name"},
+        "pyspark_job": {"main_python_file_uri": "gs://your-bucket/path/to/script.py"},
+    }
 
-    def upload_smap(**context):
-        filedate = get_target_date(context["execution_date"]).date()
-        file_uploader.upload_smap_parquet(filedate)
-
-
-    def cleanup_smap(**context):
-        filedate = get_target_date(context["execution_date"]).date()
-        file_cleaner.clean_smap(filedate)
-
-
-    def download_cygnss(**context):
-        date = get_target_date(context["execution_date"]).strftime("%Y-%m-%d")
-        cygnss_downloader.download(date, date)
-
-
-    def ingest_cygnss(**context):
-        filedate = get_target_date(context["execution_date"]).date()
-        cygnss_ingester.ingest(filedate)
-
-
-    def archive_cygnss(**context):
-        filedate = get_target_date(context["execution_date"]).date()
-        file_uploader.upload_cygnss_raw(filedate)
-
-
-    def upload_cygnss(**context):
-        filedate = get_target_date(context["execution_date"]).date()
-        file_uploader.upload_cygnss_parquet(filedate)
-
-
-    def cleanup_cygnss(**context):
-        filedate = get_target_date(context["execution_date"]).date()
-        file_cleaner.clean_cygnss(filedate)
-
-
-    # call spark cluster to run prediction
-    def predict_soil_moisture(**context):
-        filedate = get_target_date(context["execution_date"]).date()
-
-
-
-    smap_download_task = PythonOperator(task_id="download_smap", python_callable=download_smap, provide_context=True)
-    smap_ingest_task = PythonOperator(task_id=f"ingest_smap", python_callable=ingest_smap, provide_context=True)
-    smap_archive_task = PythonOperator(task_id=f"archive_smap", python_callable=archive_smap, provide_context=True)
-    smap_upload_task = PythonOperator(task_id=f"upload_smap", python_callable=upload_smap, provide_context=True)
-    smap_cleanup_task = PythonOperator(task_id=f"cleanup_smap", python_callable=cleanup_smap, provide_context=True)
-
-    cygnss_download_task = PythonOperator(task_id="download_cygnss", python_callable=download_cygnss, provide_context=True)
-    cygnss_ingest_task = PythonOperator(task_id=f"ingest_cygnss", python_callable=ingest_cygnss, provide_context=True)
-    cygnss_archive_task = PythonOperator(task_id=f"archive_cygnss", python_callable=archive_cygnss, provide_context=True)
-    cygnss_upload_task = PythonOperator(task_id=f"upload_cygnss", python_callable=upload_cygnss, provide_context=True)
-    cygnss_cleanup_task = PythonOperator(task_id=f"cleanup_cygnss", python_callable=cleanup_cygnss, provide_context=True)
-
-    smap_download_task >> smap_archive_task >> smap_cleanup_task
-    smap_download_task >> smap_ingest_task >> smap_upload_task >> smap_cleanup_task
-    cygnss_download_task >> cygnss_archive_task >> cygnss_cleanup_task
-    cygnss_download_task >> cygnss_ingest_task >> cygnss_upload_task >> cygnss_cleanup_task
-
-
-
+    submit_job = DataprocSubmitJobOperator(
+        task_id="run_spark_job",
+        job=PYSPARK_JOB,
+        region="us-west1",
+        project_id="your-project-id"
+    )
 
 
 
--- ingest/src/ingest_all.py
+++ ingest/src/ingest_all.py
@@ -7,13 +7,8 @@ import sys
 import cygnss_ingest
 import smap_ingest
 import os
-import io
 
-project_id = os.getenv("PROJECT_ID")
-subscription_id = os.getenv("SUBSCRIPTION_ID")
-schema_id = os.getenv("SCHEMA_ID")
-number_of_retries = int(os.getenv("NUMBER_OF_RETRIES"))
-topic_id = os.getenv("TOPIC_ID")
+download_date = os.getenv("DOWNLOAD_DATE")
 
 logging.basicConfig(
     level=logging.INFO,  # or DEBUG
@@ -23,66 +18,11 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
-def push_date():
-    publisher = pubsub_v1.PublisherClient()
-    topic_path = publisher.topic_path(project_id, topic_id)
-
-    client = pubsub_v1.SchemaServiceClient()
-    schema_path = client.schema_path(project_id, schema_id)
-    schema = client.get_schema(request={"name": schema_path})
-    schema_dict = json.loads(schema.definition)
-
-    # Create a record
-    record = {"DownloadDate": "2025-06-20"}
-
-    # Serialize using fastavro
-    buffer = io.BytesIO()
-    schemaless_writer(buffer, schema_dict, record)
-    avro_bytes = buffer.getvalue()
-
-    # Publish to Pub/Sub
-    print(avro_bytes.hex())
-    future = publisher.publish(topic_path, data=avro_bytes)
-    print(f"Published message ID: {future.result()}")
-
-
-def pull_date():
-    subscriber = pubsub_v1.SubscriberClient()
-    subscription_path = subscriber.subscription_path(project_id, subscription_id)
-
-    client = pubsub_v1.SchemaServiceClient()
-    schema_path = client.schema_path(project_id, schema_id)
-    schema = client.get_schema(request={"name": schema_path})
-    schema_dict = json.loads(schema.definition)
-
-    for i in range(number_of_retries):
-        response = subscriber.pull(
-            request={"subscription": subscription_path, "max_messages": 1}
-        )
-        for msg in response.received_messages:
-            try:
-                data_bytes = msg.message.data
-                bytes_io = io.BytesIO(data_bytes)
-                decoded = schemaless_reader(bytes_io, schema_dict)
-            except Exception as e:
-                logger.error(e)
-                continue
-            if "DownloadDate" in decoded:
-                subscriber.acknowledge(
-                    request={"subscription": subscription_path, "ack_ids": [msg.ack_id]}
-                )
-                datestr = decoded["DownloadDate"]
-                print(datestr)
-                return datestr
-    raise Exception(f"No download date received from pub/sub.")
-
-
 def ingest_all():
     executor = ThreadPoolExecutor(2)
-    push_date()
-    datestr = pull_date()
-    smap_exec = executor.submit(smap_ingest.ingest, datestr)
-    cygnss_exec = executor.submit(cygnss_ingest.ingest, datestr)
+
+    smap_exec = executor.submit(smap_ingest.ingest, download_date)
+    cygnss_exec = executor.submit(cygnss_ingest.ingest, download_date)
     smap_exec.result()
     cygnss_exec.result()
 
